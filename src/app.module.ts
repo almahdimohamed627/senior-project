34da2a961c02e7c99ec31d5fdbb24b8c64cb6942
MostafaAboud242 <mbwd78529@gmail.com>
2025-10-11 14:41:49 +0300

    move authentication module into backend/auth-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { FusionAuthClient } from './fusion-auth.client/fusion-auth.client';
+import { AuthModule } from 'backend/auth-module/auth/auth.module';
+import { FusionAuthClient } from 'backend/auth-module/fusion-auth.client/fusion-auth.client';
 
 @Module({
   imports: [AuthModule],
