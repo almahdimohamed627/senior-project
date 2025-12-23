0941ddab2f316b63c31ab1d112a692b1c4a260c9
Mohamed Almahdi <almahdimohamed627@gmail.com>
2025-12-23 18:00:28 +0300

    edited dirzzle.config.prod.ts

--- components/backend/drizzle.config.prod.ts
+++ components/backend/drizzle.config.prod.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: '/app/dist/src/db/schema/schema.js',
+  schema: '/app/dist/src/db/schema/*.schema.js',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: { url: process.env.DATABASE_URL! },
