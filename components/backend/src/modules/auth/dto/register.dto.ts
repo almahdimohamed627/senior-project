d867cb1ee79ad47d0a23241ddbeadc7a25aa4ac1
MostafaAboud242 <mbwd78529@gmail.com>
2025-12-27 02:02:35 +0300

    fix dto

--- components/backend/src/modules/auth/dto/register.dto.ts
+++ components/backend/src/modules/auth/dto/register.dto.ts
@@ -34,7 +34,7 @@ password: string;
 
 
   @IsNotEmpty() @IsString() gender?: string;
-  @IsNotEmpty() @IsString() city?: number;
+  @IsNotEmpty()  city?: number;
 
   // phoneNumber مطلوب لكلا الدورين بحسب منطقك السابق، لذا نحافظ عليه كحقل محتمل مطلوب
   // استخدمت IsPhoneNumber (يمكنك ضبط البلد إن أردت) — إن لم ترغب استخدم @IsString()
